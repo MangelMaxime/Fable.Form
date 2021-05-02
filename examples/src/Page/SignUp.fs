306f326147cd78c4beb50cec0186764b1fa72b94
MangelMaxime <mangel.maxime@protonmail.com>
2021-05-02 21:59:42 +0200

    Polish the SignUp example and add comments

--- examples/src/Page/SignUp.fs
+++ examples/src/Page/SignUp.fs
@@ -2,28 +2,43 @@
 
 open Elmish
 open Feliz
+open Feliz.Bulma
 open Fable.Form.Simple
 open Fable.Form.Simple.Feliz.Bulma
 
+/// <summary>
+/// Type use to represent a FieldError
+/// 
+/// It consists of two fields:
+/// - Value: which represent the value of the field when the error is to be shown
+/// - Error: The error message to display
+/// </summary>
 type FieldError =
     {
         Value : string
         Error : string
     }
 
+// Small helper module making it easier to create a FieldError
 module FieldError =
 
-    let create value error =
+    let inline create value error =
         {
             Value = value
             Error = error
         }
 
+/// <summary>
+/// Type used to represents all the external error handled by our form
+/// </summary>
 type FormErrors =
     {
         Email : FieldError option
     }
 
+/// <summary>
+/// Type used to represents the result of the form. When the form is submitted this is what we are returned
+/// </summary>
 type FormResult =
     {
         Email : EmailAddress.T
@@ -32,6 +47,9 @@ type FormResult =
         MakePublic : bool
     }
 
+/// <summary>
+/// Represents the value of the form, it also have an <c>Errors</c> field to store the external errors
+/// </summary>
 type Values =
     {
         Email : string
@@ -43,14 +61,21 @@ type Values =
     }
 
 type Model =
+    // Used when the form is being filled
     | FillingForm of Form.View.Model<Values>
+    // Used when the form has been submitted with success
     | SignedUp of User.T
 
 
 type Msg =
+    // Message to react to form change
     | FormChanged of Form.View.Model<Values>
+    // Message sent when the form is submitted
     | SignUp of FormResult
+    // Result of a sign up attenpt. It will either be Ok or contains an error message representing the external error for the email field.
     | SignupAttempted of Result<User.T, string>
+    // Message send when the user ask to reset the demo
+    | ResetTheDemo
 
 let init () =
     {
@@ -59,9 +84,9 @@ let init () =
         RepeatPassword = ""
         Name = ""
         MakePublic = ""
-        Errors = { Email = None }
+        Errors = { Email = None } // At first, there is no external error
     }
-    |> Form.View.idle
+    |> Form.View.idle // By default, set the form in idle mode
     |> FillingForm
     , Cmd.none
 
@@ -100,6 +125,7 @@ let update (msg : Msg) (model : Model) =
         SignedUp user
         , Cmd.none
 
+    // The signup attempt failed, we are going to update the external error received
     | SignupAttempted (Error error) ->
         match model with
         | FillingForm formModel ->
@@ -126,28 +152,36 @@ let update (msg : Msg) (model : Model) =
             model
             , Cmd.none
 
-let convertMakePublicOptionToBool (makePublic : string) =
+    | ResetTheDemo ->
+        init ()
+
+let private convertMakePublicOptionToBool (makePublic : string) =
     match makePublic with
     | "option-yes" -> true
     | "option-no"
     | _ -> false
 
-let form : Form.Form<Values, Msg> =
+let private form : Form.Form<Values, Msg> =
     let emailField =
         Form.textField
             {
                 Parser =
-                    EmailAddress.tryParse >> Result.map EmailAddress.toString
+                    EmailAddress.tryParse
                 Value =
                     fun values -> values.Email
                 Update =
                     fun newValue values ->
                         { values with Email = newValue }
                 Error =
+                    // Here, we have a bit of custom logic compared to the other field
+                    // because we can have external error for this field
                     fun { Email = email; Errors = errors } ->
                         match errors.Email with
+                        // If we have an external error
                         | Some emailError ->
+                            // And the value of the field is the same as the one associated with the external error
                             if email = emailError.Value then
+                                // Return the external error
                                 Some emailError.Error
                             else
                                 None
@@ -164,7 +198,7 @@ let form : Form.Form<Values, Msg> =
     let nameField =
         Form.textField
             {
-                Parser = User.Name.tryParse >> Result.map User.Name.toString
+                Parser = User.Name.tryParse
                 Value = fun values -> values.Name
                 Update =
                     fun newValue values ->
@@ -182,7 +216,7 @@ let form : Form.Form<Values, Msg> =
         Form.passwordField
             {
                 Parser =
-                    User.Password.tryParse >> Result.map User.Password.toString
+                    User.Password.tryParse
                 Value =
                     fun values -> values.Password
                 Update =
@@ -245,18 +279,16 @@ let form : Form.Form<Values, Msg> =
                     }
             }
 
-    let formOutput =
-        (fun email name password makePublic ->
-            SignUp
-                {
-                    Email = EmailAddress.create email
-                    Password = User.Password.create password
-                    Name = User.Name.create name
-                    MakePublic = convertMakePublicOptionToBool makePublic
-                }
-        )
+    let onSubmit email name password makePublic =
+        SignUp
+            {
+                Email = email
+                Password = password
+                Name = name
+                MakePublic = convertMakePublicOptionToBool makePublic
+            }
 
-    Form.succeed formOutput
+    Form.succeed onSubmit
         |> Form.append emailField
         |> Form.append nameField
         |> Form.append
@@ -268,23 +300,76 @@ let form : Form.Form<Values, Msg> =
             )
         |> Form.append makePublicField
 
+let private renderRow (leftValue : string) (rightValue : string) =
+    Html.tr [
+        Html.td leftValue
+        Html.td rightValue
+    ]
+
+let private renderSignedUpView (user : User.T) dispatch =
+    Bulma.content [
+        
+        Bulma.message [
+            color.isSuccess
+
+            prop.children [
+                Bulma.messageBody [
+                    prop.text "User signed up with the following informations"
+                ]
+            ]
+        ]      
+
+        Bulma.table [
+            table.isStriped
+
+            prop.children [
+                Html.thead [
+                    Html.tr [
+                        Html.th "Field"
+                        Html.th "Value"
+                    ]
+                ]
+
+                Html.tableBody [
+                    renderRow "Email" (User.ValidEmail.toString user.Email)
+                    renderRow "Name" (User.Name.toString user.Name)
+                    renderRow "Password" (User.Password.toString user.Password)
+                    renderRow "Is profil public?" (string user.IsProfilePublic)
+                ] 
+            ]
+
+        ]
+
+        Bulma.text.p [
+            text.hasTextCentered
+
+            prop.children [
+                Bulma.button.button [
+                    prop.onClick (fun _ -> dispatch ResetTheDemo)
+                    color.isPrimary
+
+                    prop.text "Reset the demo"
+                ]
+            ]
+        ]
+
+    ]
+     
 let view (model : Model) (dispatch : Dispatch<Msg>) =
     match model with
     | FillingForm values ->
-
         Form.View.asHtml
             {
                 Dispatch = dispatch
                 OnChange = FormChanged
                 Action = "Submit"
-                Loading = "Loading"
                 Validation = Form.View.ValidateOnSubmit
             }
             form
             values
 
     | SignedUp user ->
-        Html.text "User signed up"
+        renderSignedUpView user dispatch
 
 let code =
     """
