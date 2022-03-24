0bff73f120fcd434144d2b5bb04588cc0c698dc8
Maxime Mangel <mangel.maxime@protonmail.com>
2022-03-24 17:25:37 +0100

    Fix #31: Update commit hash for the documentation

--- examples/src/Global.fs
+++ examples/src/Global.fs
@@ -6,9 +6,9 @@ let always x _ = x
 module Env =
 
     [<Literal>]
-    let commitHash = "4deac2b86aa048545731891ec329fc4dec07fa75"
+    let commitHash = "4cb633cc1b5177632448f19cae70483dbdc8d914"
 
-    let githubBaseUrl = sprintf "https://github.com/MangelMaxime/Warded/blob/%s/" commitHash
+    let githubBaseUrl = sprintf "https://github.com/MangelMaxime/Fable.Form/blob/%s/" commitHash
 
     let generateGithubUrl (sourceDirectory : string) (fileName : string) =
         let segments = sourceDirectory.Replace("\\", "/").Split('/')
